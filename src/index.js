bf78349e5c7479890a61d4ea23d2fde4a731b07d
SatyaRac <satya.racerare87@gmail.com>
2020-04-19 14:43:45 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import "./style/style.css";
 import $ from "jquery";
 import moment from "moment";
-
+ 
 const displayTime = () => {
     moment.locale("id");
     $(".time").text(moment().format("LTS"));
