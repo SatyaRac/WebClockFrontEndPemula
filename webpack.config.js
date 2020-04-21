da13b95c313529c641515305edeee6f2cfc797b0
SatyaRac <satya.racerare87@gmail.com>
2020-04-21 11:41:50 +0700

    comiit

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
      
 module.exports = {
     entry: "./src/index.js",
@@ -35,5 +36,13 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        /* HTML Webpack Plugin */
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./src/index.html"
+            
+        })
+    ]
 }
